8ef7ed63072eba779c643b005fb50dca66c1ccb6
albertholskog <91052944+albertholskog@users.noreply.github.com>
2023-10-05 18:23:23 +0200

    add new link to github

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
         <button
           type="button"
-          onClick={() => window.open("https://github.com/albertholskog")}
+          onClick={() => window.open("https://github.com/albertholskog/ai_summarize")}
           className="black_btn"
         >
           Github
